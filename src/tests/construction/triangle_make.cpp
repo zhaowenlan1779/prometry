21241e4671de1186c69ff6e055b9aac0422c63aa
zhupengfei <zhupf321@gmail.com>
2019-09-16 22:03:13 +0800

    tests: Fill in TriangleMake test

---
+++ src/tests/construction/triangle_make.cpp
@@ -0,0 +1,26 @@
+// Copyright 2019 Zhupengfei and others
+// All rights reserved.
+
+#include <catch2/catch.hpp>
+#include "core/system.h"
+#include "geometry/construction/triangle_make.h"
+#include "geometry/element/element_types.h"
+#include "geometry/element/point.h"
+
+namespace Core {
+
+TEST_CASE("TriangleMake", "[construction]") {
+    System system;
+    system.RegisterConstruction<TriangleMake>();
+    auto p1 = system.CreateElement<Point>("", "p1");
+    auto p2 = system.CreateElement<Point>("", "p2");
+    auto p3 = system.CreateElement<Point>("", "p3");
+    auto p4 = system.CreateElement<Point>("", "p4");
+    auto p5 = system.CreateElement<Point>("", "p5");
+
+    system.Execute([](System&) { return nullptr; });
+
+    REQUIRE(system.GetElements(Elements::Triangle).size() == 60);
+}
+
+} // namespace Core
