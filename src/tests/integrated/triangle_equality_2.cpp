8b15b320bc3c5e3e50dd9b1ba7085e3828ee5152
zhupengfei <zhupf321@gmail.com>
2019-10-03 23:15:25 +0800

    Add TriangleEquality_2

--- src/tests/integrated/triangle_equality_2.cpp
+++ src/tests/integrated/triangle_equality_2.cpp
@@ -5,6 +5,7 @@
 #include "core/system.h"
 #include "geometry/construction/triangle_make.h"
 #include "geometry/element/line/line.h"
+#include "geometry/element/line/line_angle.h"
 #include "geometry/element/line/line_segment.h"
 #include "geometry/element/point.h"
 #include "geometry/transform/triangle/aa_similar.h"
@@ -14,7 +15,7 @@
 
 namespace Core {
 
-TEST_CASE("TriangleEquality_1", "[integrated]") {
+TEST_CASE("TriangleEquality_2", "[integrated]") {
     System system;
     system.RegisterTransform<AASimilar>();
     system.RegisterTransform<IsoscelesToAngles>();
@@ -27,24 +28,36 @@ TEST_CASE("TriangleEquality_1", "[integrated]") {
     auto c = system.CreateElement<Point>("", "C");
     auto d = system.CreateElement<Point>("", "D");
     auto e = system.CreateElement<Point>("", "E");
-    auto l = system.CreateElement<Line>("", "l");
-
-    d->AddParent(l);
-    b->AddParent(l);
-    c->AddParent(l);
-    e->AddParent(l);
 
     system.Algebra().AddEquation(LineSegmentLength(a, b) - LineSegmentLength(a, c));
-    system.Algebra().AddEquation(LineSegmentLength(a, d) - LineSegmentLength(a, e));
+    system.Algebra().AddEquation(LineSegmentLength(a, b) - LineSegmentLength(a, d));
+    system.Algebra().AddEquation(LineSegmentLength(a, c) - LineSegmentLength(a, e));
+    system.Algebra().AddEquation(
+        LineAngle(Line::Connect(system, a, b).first, Line::Connect(system, a, d).first).at(0) -
+        SymEngine::Expression(SymEngine::pi) / 2);
+    system.Algebra().AddEquation(
+        LineAngle(Line::Connect(system, a, c).first, Line::Connect(system, a, e).first).at(0) -
+        SymEngine::Expression(SymEngine::pi) / 2);
 
-    const auto proof = system.Execute([&b, &c, &d, &e](System& system) {
+    const auto proof1 = system.Execute([&b, &c, &d, &e](System& system) {
         const auto& [ret, proof_node] =
             system.Algebra().CheckEquation(LineSegmentLength(b, d) - LineSegmentLength(c, e));
         return ret ? proof_node : nullptr;
     });
 
-    std::cout << "Proof: " << proof << std::endl;
-    REQUIRE(!proof.empty());
+    std::cout << "Proof: " << proof1 << std::endl;
+    REQUIRE(!proof1.empty());
+
+    // const auto proof2 = system.Execute([&b, &c, &d, &e](System& system) {
+    //     const auto& [bd, dir1] = Line::Connect(system, b, d);
+    //     const auto& [bc, dir2] = Line::Connect(system, b, c);
+    //     const auto& [ret, proof_node] = system.Algebra().CheckEquation(
+    //         LineAngle(bd, dir1, bc, dir2) - 23 * SymEngine::Expression(SymEngine::pi) / 36);
+    //     return ret ? proof_node : nullptr;
+    // });
+
+    // std::cout << "Proof: " << proof2 << std::endl;
+    // REQUIRE(!proof2.empty());
 }
 
 } // namespace Core
