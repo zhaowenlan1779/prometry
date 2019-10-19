914b0c4fb134c7c8c786b3692b61352d6f5bf2d8
zhupengfei <zhupf321@gmail.com>
2019-10-19 19:09:01 +0800

    common: Removed common_funcs, rewrote assert
    Removes the codebase's dependency on GPL code.

--- src/common/assert.h
+++ src/common/assert.h
@@ -1,54 +1,55 @@
-// Copyright 2013 Dolphin Emulator Project / 2014 Citra Emulator Project
+// Copyright 2019 Zhupengfei and others
+// All rights reserved.
 
 #pragma once
 
 #include <cstdlib>
 #include <iostream>
-#include "common/common_funcs.h"
 
-// For asserts we'd like to keep all the junk executed when an assert happens away from the
-// important code in the function. One way of doing this is to put all the relevant code inside a
-// lambda and force the compiler to not inline it. Unfortunately, MSVC seems to have no syntax to
-// specify __declspec on lambda functions, so what we do instead is define a noinline wrapper
-// template that calls the lambda. This seems to generate an extra instruction at the call-site
-// compared to the ideal implementation (which wouldn't support ASSERT_MSG parameters), but is good
-// enough for our purposes.
+#ifdef _MSC_VER
+
+extern "C" {
+__declspec(dllimport) void __stdcall DebugBreak(void);
+}
+#define CRASH() DebugBreak()
+
+#else // _MSC_VER
+
+#ifdef ARCHITECTURE_x86_64
+#define CRASH() __asm__ __volatile__("int $3")
+#else
+#define CRASH() exit(1)
+#endif
+
+#endif // _MSC_VER
+
+// This is a helper to force no inline.
 template <typename Fn>
 #if defined(_MSC_VER)
-__declspec(noinline, noreturn)
+[[ noinline, noreturn ]]
 #elif defined(__GNUC__)
-    __attribute__((noinline, noreturn, cold))
+[[ noinline, noreturn, gnu::cold ]]
 #endif
-    static void assert_noinline_call(const Fn& fn) {
+    static void
+    assert_wrapper(const Fn& fn) {
     fn();
-    Crash();
-    exit(1); // Keeps GCC's mouth shut about this actually returning
+    CRASH();
+    exit(1);
 }
 
 #define ASSERT(_a_)                                                                                \
     do                                                                                             \
         if (!(_a_)) {                                                                              \
-            assert_noinline_call([] { std::cerr << "Assertion Failed!\n"; });                      \
+            assert_wrapper([] { std::cerr << "Assertion Failed!\n"; });                            \
         }                                                                                          \
     while (0)
 
 #define ASSERT_MSG(_a_, ...)                                                                       \
     do                                                                                             \
         if (!(_a_)) {                                                                              \
-            assert_noinline_call([&] { std::cerr << "Assertion Failed!\n" __VA_ARGS__ "\n"; });    \
+            assert_wrapper([&] { std::cerr << "Assertion Failed!\n" __VA_ARGS__ "\n"; });          \
         }                                                                                          \
     while (0)
 
 #define UNREACHABLE() ASSERT_MSG(false, "Unreachable code!")
 #define UNREACHABLE_MSG(...) ASSERT_MSG(false, __VA_ARGS__)
-
-#ifdef _DEBUG
-#define DEBUG_ASSERT(_a_) ASSERT(_a_)
-#define DEBUG_ASSERT_MSG(_a_, ...) ASSERT_MSG(_a_, __VA_ARGS__)
-#else // not debug
-#define DEBUG_ASSERT(_a_)
-#define DEBUG_ASSERT_MSG(_a_, _desc_, ...)
-#endif
-
-#define UNIMPLEMENTED() LOG_CRITICAL(Debug, "Unimplemented code!")
-#define UNIMPLEMENTED_MSG(_a_, ...) ASSERT_MSG(false, _a_, __VA_ARGS__)
